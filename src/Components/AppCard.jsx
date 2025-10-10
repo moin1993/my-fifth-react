f0dd1ca93aaf5fe044157b8f6b0f96ec214fa6d4
mohammad moin <moin.mohammad.uddin@gmail.com>
2025-10-10 19:32:53 +0600

    navbar download image set

--- src/Components/AppCard.jsx
+++ src/Components/AppCard.jsx
@@ -7,7 +7,7 @@ const AppCard = ({ app }) => {
       <div className=" p-4">
         <Link
           to={`/Apps/${app.id}`}
-          className="border-gray-500 border-2 card bg-base-100 w-55 p-4 gap-5 shadow-sm hover:scale-110 transition ease-in-out"
+          className="border-gray-500 border-2 card bg-base-100 w-70 p-4 gap-5 shadow-sm hover:scale-110 transition ease-in-out"
         >
           <figure className=" h-48 overflow-hidden">
             <img src={app.image} alt="Shoes" className="w-full object-cover " />
@@ -15,16 +15,20 @@ const AppCard = ({ app }) => {
           <div className="card-body">
             <h2 className="card-title">{app.title}</h2>
 
-            <div className="flex justify-between ">
-              <div>
-                <p className="text-green-600 bg-gray-300 font-bold p-2 rounded-2xl">
+            <div className="flex justify-between items-center ">
+              <div className="flex justify-center items-center bg-yellow-100 h-fit p-2 rounded-2xl">
+                <img
+                  className="h-5"
+                  src="/public/assets/icon-ratings.png"
+                  alt=""
+                />
+                <p className="text-green-600 font-bold p-2 rounded-2xl">
                   {app.ratingAvg}
                 </p>
               </div>
-              <div>
-                <p className="text-purple-800 bg-gray-300 font-bold p-2 rounded-2xl">
-                  {app.downloads}
-                </p>
+              <div className="flex justify-center items-center bg-gray-300 p-2 rounded-2xl h-fit">
+                <img className="h-5" src="/assets/icon-downloads.png" alt="" />
+                <p className="text-purple-800 font-bold ">{app.downloads}</p>
               </div>
             </div>
           </div>
